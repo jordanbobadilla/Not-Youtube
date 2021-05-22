35fcef78b5a3c68393eaa450e77850046e52401b
Jordan Bobadilla <jordanboba08@gmail.com>
2021-05-22 18:33:19 -0400

    styling the navBar

--- our-youtube-app/src/Components/NavBar.js
+++ our-youtube-app/src/Components/NavBar.js
@@ -1,18 +1,24 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
-import React from 'react';
+import React from "react";
+import Logo from "../images/Group 1.svg";
 
+const Navbar = () => {
+  return (
+    <header className="NavContainer">
+      <nav className="first-nav">
+        <img src={Logo} alt="Not-Youtube-logo" />
+        <p>NotYouTube</p>
+      </nav>
 
-const Navbar = () =>{
-    return(
-        <nav className="NavContainer">
-            <NavLink to={"/"}>
-
-            </NavLink>
-            <NavLink exact to="/Home">Home</NavLink>
-            <NavLink to="/about">About Us</NavLink>
-        </nav>
-    );
+      <nav className="sec-nav">
+        <NavLink exact to="/Home">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="about">About Us</NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
